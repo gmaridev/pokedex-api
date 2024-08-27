0a4877037bd6ffcd45cbce55146b204d9942c560
Maria Gabriela Santillan <mari.cabsantillan@gmail.com>
2024-08-26 21:56:08 -0500

    icons

--- src/components/icons/icon-type.js
+++ src/components/icons/icon-type.js
@@ -1,19 +1,61 @@
+import fireIcon from '../assets/fire.svg';
+import waterIcon from '../assets/water.svg';
+import grassIcon from '../assets/grass.svg';
+import electricIcon from '../assets/electric.svg';
+import bugIcon from '../assets/bug.svg';
+import ghostIcon from '../assets/ghost.svg';
+import dragonIcon from '../assets/dragon.svg';
+import fightingIcon from '../assets/fighting.svg';
+import iceIcon from '../assets/ice.svg';
+import flyingIcon from '../assets/flying.svg';
+import poisonIcon from '../assets/poison.svg';
+import psychicIcon from '../assets/psychic.svg';
+import darkIcon from '../assets/dark.svg';
+import fairyIcon from '../assets/fairy.svg';
+import rockIcon from '../assets/rock.svg';
+import groundIcon from '../assets/ground.svg';
+
 const typeIcons = {
-    fire: '../src/assets/fire.svg',
-    water: '../src/assets/water.svg',
-    grass: '../src/assets/grass.svg',
-    electric: '../src/assets/electric.svg',
-    bug: '../src/assets/bug.svg',
-    ghost: '../src/assets/ghost.svg',
-    dragon: '../src/assets/dragon.svg',
-    fighting: '../src/assets/fighting.svg',
-    ice: '../src/assets/ice.svg',
-    flying: '../src/assets/flying.svg',
-    poison: '../src/assets/poison.svg',
-    psychic: '../src/assets/psychic.svg',
-    dark: '../src/assets/dark.svg',
-    fairy: '../src/assets/fairy.svg',
-    rock: '../src/assets/rock.svg',
-    ground: '../src/assets/ground.svg',
+    fire: fireIcon,
+    water: waterIcon,
+    grass: grassIcon,
+    electric: electricIcon,
+    bug: bugIcon,
+    ghost: ghostIcon,
+    dragon: dragonIcon,
+    fighting: fightingIcon,
+    ice: iceIcon,
+    flying: flyingIcon,
+    poison: poisonIcon,
+    psychic: psychicIcon,
+    dark: darkIcon,
+    fairy: fairyIcon,
+    rock: rockIcon,
+    ground: groundIcon,
 };
-export default typeIcons
+
+export default typeIcons;
+
+
+
+
+
+// const typeIcons = {
+//     fire: '../src/assets/fire.svg',
+//     water: '../src/assets/water.svg',
+//     grass: '../src/assets/grass.svg',
+//     electric: '../src/assets/electric.svg',
+//     bug: '../src/assets/bug.svg',
+//     ghost: '../src/assets/ghost.svg',
+//     dragon: '../src/assets/dragon.svg',
+//     fighting: '../src/assets/fighting.svg',
+//     ice: '../src/assets/ice.svg',
+//     flying: '../src/assets/flying.svg',
+//     poison: '../src/assets/poison.svg',
+//     psychic: '../src/assets/psychic.svg',
+//     dark: '../src/assets/dark.svg',
+//     fairy: '../src/assets/fairy.svg',
+//     rock: '../src/assets/rock.svg',
+//     ground: '../src/assets/ground.svg',
+// };
+// export default typeIcons
